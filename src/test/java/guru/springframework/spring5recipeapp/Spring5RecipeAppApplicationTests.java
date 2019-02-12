5e82b82c34c2f874df7b7489bb79371fbd078909
terryliu1995 <zliu48@ncsu.edu>
2019-02-12 00:59:59 -0500

    init and finish ingredient notes and recipe entity

---
+++ src/test/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,17 @@
+package guru.springframework.spring5recipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5RecipeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
